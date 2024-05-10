38d3c1f3711e9633520567774e5e183808d591df
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-10 18:20:28 +0000

    done for today

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function divideNumbers() {
+  const dividend = parseInt(document.getElementById('dividendInput').value);
+  let divisor = parseInt(document.getElementById('divisorInput').value);
+
+  let quotient = 0;
+  let remainder = dividend;
+
+  while (remainder >= divisor) {
+    remainder -= divisor;
+    quotient++;
+  }
+
+  document.getElementById("divisionResult").innerHTML = "<p>" + dividend + " / " + divisor + " = " + quotient + " R " + remainder + "</p>";
+}
